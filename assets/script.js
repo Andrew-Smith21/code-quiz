54e693034580c7da38c19e465d179e10525c5609
Chad Smith <chad.smith@vanderbilt.edu>
2022-05-09 21:19:05 -0400

    setting up start function

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+
+var startquizEl = document.querySelector("#start-quiz");
+var openingpageEl = document.querySelector("#opening-page");
+console.log(startquizEl);
+console.log(openingpageEl);
+
+
+
+
+
+
+
+function startQuiz() {
+
+    openingpageEl.style.display = "none";
+
+    // Create elements for first question
+    // Append elements to to <main>
+}
+
+
+
+
+
+
+
+
+
+
+startquizEl.addEventListener("click", startQuiz);
